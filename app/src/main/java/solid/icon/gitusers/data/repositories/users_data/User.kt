98e06bdb6f5308de8aa2e09cfea0d1e42e296fce
OlegKarmazyn <ol0321k@gmail.com>
2023-06-21 12:22:30 +0300

    removed unnecessary variables

--- app/src/main/java/solid/icon/gitusers/data/repositories/users_data/User.kt
+++ app/src/main/java/solid/icon/gitusers/data/repositories/users_data/User.kt
@@ -3,8 +3,8 @@ package solid.icon.gitusers.data.repositories.users_data
 data class User(
     val login: String,
     val id: Int,
-    val avatar_url: String,
-    val url: String,
-    val html_url: String,
-    val repos_url: String
+    val avatar_url: String
+    //val url: String,
+    //val html_url: String,
+    //val repos_url: String
 )
